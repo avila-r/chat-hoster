f792e3f3eeecb4600f7dd5cc56c2cf5ea4687f05
avila-r <avila.dev@outlook.com>
2024-09-10 20:27:07 -0300

    feat: implement service layer

--- internal/users/user_service.go
+++ internal/users/user_service.go
@@ -9,9 +9,32 @@ func NewService(r Repository) *Service {
 }
 
 func (s *Service) Register(r *RegisterRequest) (*RegisterResponse, error) {
-	return nil, nil
+	u := &User{
+		Username: r.Username,
+		Email:    r.Email,
+		Password: r.Password,
+	}
+
+
+	// TODO: Error handling
+	result, _ := s.r.CreateUser(u)
+
+	response := &RegisterResponse{
+		ID:       int64(result.ID),
+		Username: result.Email,
+		Email:    result.Password,
+	}
+
+	return response, nil
 }
 
 func (s *Service) Login(r *LoginRequest) (*LoginResponse, error) {
-	return nil, nil
+	// TODO
+	response := LoginResponse{
+		Token:    "access_token",
+		ID:       1,
+		Username: "username",
+	}
+
+	return &response, nil
 }
