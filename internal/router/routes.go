808aca7a222fdc9c47b50545153e1fef1f730c61
avila-r <avila.dev@outlook.com>
2024-09-12 20:14:41 -0300

    feat: implement ws routes

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/avila-r/chat-hoster/db"
+	"github.com/avila-r/chat-hoster/internal/chat"
 	"github.com/avila-r/chat-hoster/internal/users"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -11,12 +12,18 @@ var (
 	user_repository users.Repository
 	user_service    users.Service
 	user_handler    users.Handler
+	server          chat.Server
+	chat_handler    chat.Handler
 )
 
 func setup() {
 	user_repository = *users.NewRepository(db.GetConnection())
 	user_service = *users.NewService(user_repository)
 	user_handler = *users.NewHandler(user_service)
+
+	server = *chat.NewServer()
+	chat_handler = *chat.NewHandler(&server)
+	go server.Run()
 }
 
 func EnableRouting(app *fiber.App) {
@@ -27,9 +34,40 @@ func EnableRouting(app *fiber.App) {
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	app.Post("/register", user_handler.CreateUser)
-	app.Post("/login", user_handler.Login)
-	app.Post("/logout", user_handler.Logout)
+	app.Post(
+		"/register",
+		user_handler.CreateUser,
+	)
+
+	app.Post(
+		"/login",
+		user_handler.Login,
+	)
+
+	app.Post(
+		"/logout",
+		user_handler.Logout,
+	)
+
+	app.Get(
+		"/clients/:room_id",
+		chat_handler.GetClients,
+	)
+
+	app.Get(
+		"/rooms",
+		chat_handler.GetRooms,
+	)
+
+	app.Post(
+		"/rooms/:room_id",
+		chat_handler.JoinRoom,
+	)
+
+	app.Post(
+		"/rooms",
+		chat_handler.CreateRoom,
+	)
 
 	_ = app.Listen(":8888")
 }
