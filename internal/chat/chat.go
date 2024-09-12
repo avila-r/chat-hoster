41946a83e96f8e186106961f21720a5b374c5e3e
avila-r <avila.dev@outlook.com>
2024-09-12 20:13:25 -0300

    feat: implement better error handling

--- internal/chat/chat.go
+++ internal/chat/chat.go
@@ -1,16 +1,22 @@
 package chat
 
 import (
+	"log"
+	"sync"
+
 	http "github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 )
 
 type Handler struct {
 	server *Server
+	mutex  sync.RWMutex
 }
 
 func NewHandler(s *Server) *Handler {
-	return &Handler{s}
+	return &Handler{
+		server: s,
+	}
 }
 
 func (h *Handler) CreateRoom(c *http.Ctx) error {
@@ -25,7 +31,16 @@ func (h *Handler) CreateRoom(c *http.Ctx) error {
 
 	if err := c.BodyParser(&request); err != nil {
 		return c.Status(http.StatusBadRequest).JSON(http.Map{
-			"error": err.Error(),
+			"error": "failed to parse request body - " + err.Error(),
+		})
+	}
+
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	if _, exists := h.server.Rooms[request.ID]; exists {
+		return c.Status(http.StatusConflict).JSON(http.Map{
+			"error": "room already exists",
 		})
 	}
 
@@ -35,37 +50,47 @@ func (h *Handler) CreateRoom(c *http.Ctx) error {
 		Clients: make(map[string]*Client),
 	}
 
-	return c.JSON(http.StatusCreated)
+	log.Printf("Room created: %s", request.Name)
+
+	return c.Status(http.StatusCreated).JSON(http.Map{
+		"id":   request.ID,
+		"name": request.Name,
+	})
 }
 
 func (h *Handler) JoinRoom(c *http.Ctx) error {
 	if !websocket.IsWebSocketUpgrade(c) {
 		return c.Status(http.StatusUpgradeRequired).JSON(http.Map{
-			"error": "This endpoint requires a WebSocket connection",
+			"error": "this endpoint requires a web socket connection",
 		})
 	}
 
 	return websocket.New(func(connection *websocket.Conn) {
-		roomID := c.Params("roomId")
-		clientID := c.Query("userId")
+		room_id := c.Params("room_id")
+		client_id := c.Query("user_id")
 		username := c.Query("username")
 
 		client := &Client{
 			Connection: connection,
 			Message:    make(chan *Message, 10),
-			ID:         clientID,
-			RoomID:     roomID,
+			ID:         client_id,
+			RoomID:     room_id,
 			Username:   username,
 		}
 
 		m := &Message{
 			Content:  "A new user has joined the room",
-			RoomID:   roomID,
+			RoomID:   room_id,
 			Username: username,
 		}
 
 		h.server.Register <- client
 		h.server.Broadcast <- m
+
+		log.Printf(
+			"User %s joined room %s",
+			username, room_id,
+		)
 
 		go client.Write()
 		client.Read(h.server)
@@ -79,8 +104,18 @@ func (h *Handler) GetRooms(c *http.Ctx) error {
 	}
 
 	var (
-		rooms []RoomResponse
+		rooms = make([]RoomResponse, 0)
 	)
+
+	h.mutex.RLock()
+	defer h.mutex.RUnlock()
+
+	for _, r := range h.server.Rooms {
+		rooms = append(rooms, RoomResponse{
+			ID:   r.ID,
+			Name: r.Name,
+		})
+	}
 
 	return c.Status(http.StatusOK).JSON(rooms)
 }
@@ -92,8 +127,26 @@ func (h *Handler) GetClients(c *http.Ctx) error {
 	}
 
 	var (
-		clients []ClientResponse
+		clients = make([]ClientResponse, 0)
 	)
+
+	h.mutex.RLock()
+	defer h.mutex.RUnlock()
+
+	room, ok := h.server.Rooms[c.Params("room_id")]
+
+	if !ok {
+		return c.Status(http.StatusNotFound).JSON(http.Map{
+			"error": "room not found",
+		})
+	}
+
+	for _, c := range room.Clients {
+		clients = append(clients, ClientResponse{
+			ID:       c.ID,
+			Username: c.Username,
+		})
+	}
 
 	return c.Status(http.StatusOK).JSON(clients)
 }
